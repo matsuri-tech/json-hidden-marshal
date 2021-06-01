69e75d7893bd18c791b028b96b87407d05987b07
myuon <ioi.joi.koi.loi@gmail.com>
2021-06-01 17:54:47 +0900

    test: more tests

--- marshal_test.go
+++ marshal_test.go
@@ -5,35 +5,61 @@ import (
 	"testing"
 )
 
-type User struct {
-	Name     string `json:"name"`
-	Name2    string `json:"name2" hidden:"-"`       // skip
-	Name3    string `json:"name3" hidden:"true"`    // skip
-	Password string `json:"password" hidden:"mask"` // masked
-	Nested   UserNested
-}
-
-type UserNested struct {
-	Open   string
-	Hidden string `hidden:"-"`
-}
-
 func TestMarshal(t *testing.T) {
-	user := User{
-		Name:     "name",
-		Name2:    "name2",
-		Name3:    "name3",
-		Password: "password",
-		Nested: UserNested{
-			Open:   "open",
-			Hidden: "in",
+	cases := []struct {
+		in       interface{}
+		expected string
+	}{
+		{
+			// hidden:- or hidden:true to skip, hidden:mask to mask
+			in: struct {
+				Name     string `json:"name"`
+				Name2    string `json:"name2" hidden:"-"`       // skip
+				Name3    string `json:"name3" hidden:"true"`    // skip
+				Password string `json:"password" hidden:"mask"` // masked
+			}{
+				Name:     "foo",
+				Password: "password",
+			},
+			expected: `{"name":"foo","password":"********"}`,
+		},
+		{
+			// nested type
+			in: struct {
+				Nested struct {
+					Open   string `json:"open"`
+					Hidden string `json:"hidden" hidden:"-"`
+				} `json:"nested"`
+			}{
+				Nested: struct {
+					Open   string `json:"open"`
+					Hidden string `json:"hidden" hidden:"-"`
+				}{
+					Open:   "open",
+					Hidden: "hidden",
+				},
+			},
+			expected: `{"nested":{"open":"open"}}`,
+		},
+		{
+			// Without json tags
+			in: struct {
+				Name     string
+				Password string `hidden:"-"`
+			}{
+				Name:     "foo",
+				Password: "password",
+			},
+			expected: `{"Name":"foo"}`,
 		},
 	}
 
-	out, err := Marshal(user)
-	if err != nil {
-		t.Errorf("%+v\n", err)
-	}
+	for _, c := range cases {
+		out, err := Marshal(c.in)
+		if err != nil {
+			t.Errorf("%+v\n", err)
+		}
 
-	assert.JSONEq(t, string(out), `{"name":"name","password":"********","Nested":{"Open":"open"}}`)
+		assert.JSONEq(t, string(out), c.expected)
+	}
 }
